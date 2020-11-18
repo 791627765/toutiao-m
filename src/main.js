40c69c1e9ec738d9915022d569efb3e44f666314
武志斌 <791627765@qq.com>
2020-11-18 11:30:13 +0800

    初始化了项目页面

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import './styles/index.less'
+//加载vant核心组件库
+import Vant from 'vant'
+//加载want全局样式
+import 'vant/lib/index.css'
+//注册使用vant
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
